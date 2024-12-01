78411ecb13f149b14a4b1c7406951091caa5db4b
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-12-01 09:00:26 -0500

    flask server file now passing pylint checks

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This module provides a flask server and form to detect emotions in text.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +9,22 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the emotion detection form
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """
+    Detect emotions in some text
+
+    Args:
+        textToAnalyze (str): The text to analyze
+
+    Returns:
+        A JSON response with the emotions and their scores.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -21,12 +37,13 @@ def emot_detector():
     sadness = emotions["sadness"]
     dominant_emotion = emotions["dominant_emotion"]
 
-    if (dominant_emotion is None):
+    if dominant_emotion is None:
         return "Invalid text! Please try again!"
 
     response = (
         f"For the given statement, the system response is "
-        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} "
+        f"and 'sadness': {sadness}. "
         f"The dominant emotion is {dominant_emotion}."
     )
 
